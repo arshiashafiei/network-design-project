eda5d68fc4796cb449668fb9e167d3ca8590e2ea
Arshia Shafiei <arshiashafiei03@gmail.com>
2024-12-13 14:36:41 +0330

    Fix sender code

--- main.py
+++ main.py
@@ -13,23 +13,33 @@ def read_file_and_send(filename, source_ip, destination_ip):
 
             # Send the packet to the Dest.
             send(outer_ip_packet)
+            print(outer_ip_packet)
 
 
-def receive_and_process_packets(interface, expected_src_ip):
-    if IP in packet and packet[IP].src == expected_src_ip:
-        outer_payload = packet[IP].payload
-        if IP in outer_payload:
-            inner_ip_packet = outer_payload[IP]
-            if Raw in inner_ip_packet:
-                payload_content = inner_ip_packet[Raw].load.decode('utf-8')
-                print(f"Received line: {payload_content}")
+def receive_and_process_packets(packet, expected_src_ip):
+    print(f"Processing packet: {packet}")
+    if IP in packet and packet[IP].dst == expected_src_ip:
+        payload = packet[IP].payload
+        if Raw in payload:
+            payload_content = payload[Raw].load.decode('utf-8')
+            print(f"Received line: {payload_content}")
+    else:
+        print("Packet is not the one we want! :/")
+
+        # outer_payload = packet[IP].payload
+        # if IP in outer_payload:
+        #     inner_ip_packet = outer_payload[IP]
+        #     if Raw in inner_ip_packet:
+        #         payload_content = inner_ip_packet[Raw].load.decode('utf-8')
+        #         print(f"Received line: {payload_content}")
 
 
 def listener(interface, expected_src_ip):
-    Captured = sniff(iface=interface, filter=f"ip src {expected_src_ip}", count=1)
-    print(f"Captured packet: {Captured}")
+    while True:
+        Captured = sniff(iface=interface, filter=f"ip dst {expected_src_ip}", count=1)
+        print(f"Captured packet: {Captured}")
 
-    return Captured
+        receive_and_process_packets(Captured[0], expected_src_ip)
 
 
 def start_receiver(interface, expected_src_ip):
@@ -40,12 +50,21 @@ def start_receiver(interface, expected_src_ip):
 def main():
     mode = input("Select mode(1: sender, 2: Receiver): ")
     filename = "send.txt"
-    source_ip = ""
-    destination_ip = ""
-    interface = ""
+    source_ip = "192.168.74.190"
+    destination_ip = "192.168.74.190"
+    interface = "wlp0s20f3"
+    start_receiver(interface, source_ip)
 
     if mode == "1":
         read_file_and_send(filename, source_ip, destination_ip)
+
+        # Keep the main thread alive
+        try:
+            while True:
+                pass
+        except KeyboardInterrupt:
+            print("Receiver stopped.")
+
     elif mode == "2":
         start_receiver(interface, destination_ip)
 
